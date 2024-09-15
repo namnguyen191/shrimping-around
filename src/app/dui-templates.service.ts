13de4e2198ddd4e42e7b8898e1c7e37f5e9079d2
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-09-15 00:16:03 -0400

    fix: use base href when fetching assets in service

--- src/app/dui-templates.service.ts
+++ src/app/dui-templates.service.ts
@@ -1,3 +1,4 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import {
@@ -11,19 +12,20 @@ import {
 })
 export class DUITemplatesService {
   readonly #httpClient = inject(HttpClient);
+  readonly #appBaseHref = inject(APP_BASE_HREF, { optional: true }) ?? '';
 
   getLayoutTemplate = (id: string) =>
     this.#httpClient.get<LayoutTemplate>(
-      `/dui-assets/layout-templates/${id}.json`
+      `${this.#appBaseHref}dui-assets/layout-templates/${id}.json`
     );
 
   getUiElementTemplate = (id: string) =>
     this.#httpClient.get<UIElementTemplate>(
-      `/dui-assets/ui-element-templates/${id}.json`
+      `${this.#appBaseHref}dui-assets/ui-element-templates/${id}.json`
     );
 
   getRemoteResourceTemplate = (id: string) =>
     this.#httpClient.get<RemoteResourceTemplate>(
-      `/dui-assets/remote-resource-templates/${id}.json`
+      `${this.#appBaseHref}dui-assets/remote-resource-templates/${id}.json`
     );
 }
